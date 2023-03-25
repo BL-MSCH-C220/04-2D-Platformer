1adaa4ab2460c6dc11c171926588bd13c62c704a
BL-MSCH-C220-F22 <121530054+BL-MSCH-C220@users.noreply.github.com>
2023-03-25 07:46:16 -0400

    Completes demonstration

--- Player/Player.gd
+++ Player/Player.gd
@@ -4,6 +4,7 @@ onready var SM = $StateMachine
 
 var velocity = Vector2.ZERO
 var jump_power = Vector2.ZERO
+var djump_power = Vector2.ZERO
 var direction = 1
 
 export var gravity = Vector2(0,30)
@@ -12,16 +13,16 @@ export var move_speed = 20
 export var max_move = 300
 
 export var jump_speed = 200
-export var max_jump = 1200
+export var max_jump = 500
 
-export var leap_speed = 200
-export var max_leap = 1200
+export var jump_forgiveness = 10
 
 var moving = false
 var is_jumping = false
+var jump_reset = true
 var double_jumped = false
 var should_direction_flip = true # wether or not player controls (left/right) can flip the player sprite
-
+var just_jumped = 0
 
 func _physics_process(_delta):
 	velocity.x = clamp(velocity.x,-max_move,max_move)
@@ -32,7 +33,15 @@ func _physics_process(_delta):
 	
 	if is_on_floor():
 		double_jumped = false
+		jump_power = Vector2.ZERO
 		set_wall_raycasts(true)
+	if not Input.is_action_pressed("jump"): 
+		just_jumped = 0
+		jump_reset = true
+	else: 
+		just_jumped += 1
+	if just_jumped < jump_forgiveness and is_on_floor():
+		jump_reset = true
 
 func is_moving():
 	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
--- StateMachine/Coyote.gd
+++ StateMachine/Coyote.gd
@@ -10,17 +10,10 @@ func start():
 	$Timer.start()
 
 func physics_process(_delta):
-	player.jump_power.y = clamp(player.jump_power.y - player.jump_speed, -player.max_jump, 0)
-	if Input.is_action_just_released("jump"):
-		player.velocity.y = 0
-		player.velocity += player.jump_power
-		player.move_and_slide(player.velocity, Vector2.UP)
-		SM.set_state("Falling")
-	elif player.is_moving():
-		player.set_animation("Moving")
-		player.velocity += player.move_speed * player.move_vector()
+	if Input.is_action_pressed("jump") and player.jump_reset:
+		SM.set_state("Jumping")
 	else:
-		player.velocity.x = 0
+		SM.set_state("Falling")
 
 
 func _on_Timer_timeout():
